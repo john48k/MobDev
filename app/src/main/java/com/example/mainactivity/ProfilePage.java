1840d2b530a642c3e544c96028d86d835a56294c
rychfghg <bacolod2186@gmail.com>
2024-05-27 14:09:48 +0800

    update

--- app/src/main/java/com/example/mainactivity/ProfilePage.java
+++ app/src/main/java/com/example/mainactivity/ProfilePage.java
@@ -1,6 +1,10 @@
 package com.example.mainactivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ProfilePage extends AppCompatActivity {
 
+    Button b1, b2, b3, b4, b5, b6, b7, b8;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnprofile), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        b1 = findViewById(R.id.buttonDirectMessage);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(ProfilePage.this, signup.class);
+                startActivity(intent1);
+            }
+        });
+
+        b2 = findViewById(R.id.buttonBrowse);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(ProfilePage.this, product.class);
+                startActivity(intent2);
+            }
+        });
+
+        b3 = findViewById(R.id.buttonLogout);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(ProfilePage.this, login.class);
+                startActivity(intent2);
+            }
         });
     }
 }
